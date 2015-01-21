964d2807734e904f99fe0019afd571dbdaa89947
Joshua <jdong42@gmail.com>
2015-01-20 22:03:07 -0600

    Updated repo to not suck, and swung.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,16 +1,53 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class Hello extends JPanel implements ActionListener {
 
-public class Hello {
-
-	public static void main(String[] args) {
-	    JFrame.setDefaultLookAndFeelDecorated(true);
-		JFrame frame = new JFrame("Hello World.");
-		JLabel label = new JLabel("Hello, Anon.");
-		JLabel label = new JLabel("Hello World");
-	    frame.getContentPane().add(label);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.pack();
-	    frame.setVisible(true);
-	}
+	private static final long serialVersionUID = 1L;
+	protected JTextField textIn;
+    protected JLabel textOut;
+    private final static String newline = "\n";
+ 
+    public Hello() {
+        super(new GridBagLayout());
+ 
+        textIn = new JTextField(20);
+        textIn.addActionListener(this);
+ 
+        textOut = new JLabel("Hello, Anon.");
+ 
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textIn, c);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        add(textOut, c);
+    }
+ 
+    public void actionPerformed(ActionEvent evt) {
+        textOut.setText("Hello, " + textIn.getText() + ".");
+        textIn.selectAll();
+    }
+ 
+    // For thread safety, invoke from the event dispatch thread.
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("EE422C Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        frame.add(new Hello());
+ 
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
