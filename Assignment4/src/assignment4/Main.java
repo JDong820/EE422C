afa6ee65d1b17625d7ee94951bc669879cf50d08
Joshua <jdong42@gmail.com>
2015-04-05 21:38:37 -0500

    Added exp6

--- Assignment4/src/assignment4/Main.java
+++ Assignment4/src/assignment4/Main.java
@@ -8,12 +8,12 @@ package assignment4;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Set;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
-//import java.util.HashMap;
 //import java.util.Map.Entry;
 //import static java.util.stream.Collectors.toMap;
 
@@ -29,24 +29,22 @@ import java.lang.NumberFormatException;
 
 class Main {
     static int COUNT = 1000000;
+    static int MAX_VAL = Integer.MAX_VALUE;
     static boolean doDebug = false;
+    static String FILENAME = "input.txt";
 
     public static void main(String[] args) {
         if (args.length == 1) {
-            COUNT = Integer.parseInt(args[0]);
-        }
-        if (args.length == 2) {
-            COUNT = Integer.parseInt(args[0]);
-            doDebug = true;
+            FILENAME = args[0];
         }
 
         Random rand = new Random(15485863);
         ArrayList<Integer> data = new ArrayList(
-                Arrays.stream(new Random()
-                    .ints(COUNT, 1, Integer.MAX_VALUE)
-                    .toArray())
-                .boxed()
-                .collect(Collectors.toList()));
+            Arrays.stream(new Random()
+                          .ints(COUNT, 1, MAX_VAL)
+                          .toArray())
+            .boxed()
+            .collect(Collectors.toList()));
 
         if (doDebug) {
             debug(data);
@@ -58,14 +56,15 @@ class Main {
             experiment3(data);
             experiment4(data);
             experiment5(data);
+            experiment6(data);
         } catch (IOException e) {
             System.out.println(e);
         }
     }
-    
+
     // Returns index of target in sortedList, or -1 if not found.
     public static int interpolationSearch(List<Integer> sortedList,
-            Integer target) {
+                                          Integer target) {
         int low = 0;
         int high = sortedList.size() - 1;
         int mid;
@@ -73,8 +72,8 @@ class Main {
         while (sortedList.get(low) <= target &&
                 sortedList.get(high) >= target) {
             mid = low +
-                ((target - sortedList.get(low)) * (high - low)) /
-                (sortedList.get(high) - sortedList.get(low));
+                  ((target - sortedList.get(low)) * (high - low)) /
+                  (sortedList.get(high) - sortedList.get(low));
 
             if (sortedList.get(mid) < target) {
                 low = mid + 1;
@@ -83,7 +82,7 @@ class Main {
             } else {
                 return mid;
             }
-                }
+        }
 
         if (sortedList.get(low) == target) {
             return low;
@@ -93,25 +92,25 @@ class Main {
     }
 
     public static void makeOutput(int n, long found, long notFound,
-            long invalidCount, StopWatch watch) {
+                                  long invalidCount, StopWatch watch) {
         System.out.println("Experiment " + n + ":"
-                + "\nNumber of found values: " + found
-                + "\nNumber of values not found: " + notFound
-                + "\nNumber of illegal values in input file: " + invalidCount
-                + "\nTime elapsed in nanoseconds is: "
-                + watch.getElapsedNanoseconds()
-                + "\nTime elapsed in seconds is: " 
-                + watch.getElapsedSeconds());
+                           + "\nNumber of found values: " + found
+                           + "\nNumber of values not found: " + notFound
+                           + "\nNumber of illegal values in input file: " + invalidCount
+                           + "\nTime elapsed in nanoseconds is: "
+                           + watch.getElapsedNanoseconds()
+                           + "\nTime elapsed in seconds is: "
+                           + watch.getElapsedSeconds());
     }
 
     public static void makeOutput(int n, StopWatch watch) {
         System.out.println("Experiment " + n + ":"
-                + "\nTime elapsed in nanoseconds is: "
-                + watch.getElapsedNanoseconds()
-                + "\nTime elapsed in seconds is: " 
-                + watch.getElapsedSeconds());
+                           + "\nTime elapsed in nanoseconds is: "
+                           + watch.getElapsedNanoseconds()
+                           + "\nTime elapsed in seconds is: "
+                           + watch.getElapsedSeconds());
     }
-   
+
     public static Integer s2i(String s) {
         try {
             Integer value = Integer.parseInt(s);
@@ -122,11 +121,11 @@ class Main {
     }
 
     public static List<Integer> readInput() throws IOException {
-        Path path = Paths.get(System.getProperty("user.dir"), "input.txt");
-        try (Stream<String> lines = Files.lines(path)){
+        Path path = Paths.get(System.getProperty("user.dir"), FILENAME);
+        try (Stream<String> lines = Files.lines(path)) {
             return lines
-                .map(s -> s2i(s))
-                .collect(Collectors.toList());
+                   .map(s -> s2i(s))
+                   .collect(Collectors.toList());
         }
     }
 
@@ -140,27 +139,28 @@ class Main {
      * when done.
      */
     public static void experiment1(ArrayList<Integer> data)
-        throws IOException {
+    throws IOException {
         List<Integer> raw = readInput();
         long invalidCount = raw
-            .stream()
-            .filter(i -> i == null)
-            .count();
+                            .stream()
+                            .filter(i -> i == null)
+                            .count();
         List<Integer> targets = raw
-            .stream()
-            .filter(i -> i != null)
-            .collect(Collectors.toList());
+                                .stream()
+                                .filter(i -> i != null)
+                                .collect(Collectors.toList());
         int count = 0;
         StopWatch watch = new StopWatch();
 
         for (Integer target: targets) {
             watch.start();
-            if (data.contains(target)) {
+            boolean isContained = data.contains(target);
+            watch.stop();
+            if (isContained) {
                 ++count;
             }
-            watch.stop();
         }
-        
+
         makeOutput(1, count, targets.size() - count, invalidCount, watch);
     }
 
@@ -174,96 +174,137 @@ class Main {
      * when done.
      */
     public static void experiment2(ArrayList<Integer> data)
-        throws IOException {
+    throws IOException {
         List<Integer> raw = readInput();
         long invalidCount = raw
-            .stream()
-            .filter(i -> i == null)
-            .count();
+                            .stream()
+                            .filter(i -> i == null)
+                            .count();
         List<Integer> targets = raw
-            .stream()
-            .filter(i -> i != null)
-            .collect(Collectors.toList());
+                                .stream()
+                                .filter(i -> i != null)
+                                .collect(Collectors.toList());
         int count = 0;
         StopWatch watch = new StopWatch();
         LinkedList<Integer> newData = new LinkedList(data);
 
         for (Integer target: targets) {
             watch.start();
-            if (newData.contains(target)) {
+            boolean isContained = newData.contains(target);
+            watch.stop();
+            if (isContained) {
                 ++count;
             }
-            watch.stop();
         }
-        
+
         makeOutput(2, count, targets.size() - count, invalidCount, watch);
     }
 
     // Sort a list. Time it.
     public static void experiment3(ArrayList<Integer> data)
-        throws IOException {
+    throws IOException {
         StopWatch watch = new StopWatch();
 
         watch.start();
         Collections.sort(data);
         watch.stop();
-        
+
         makeOutput(3, watch);
     }
 
     /* Use binary search. Time it.
      */
     public static void experiment4(ArrayList<Integer> data)
-        throws IOException {
+    throws IOException {
         List<Integer> raw = readInput();
         long invalidCount = raw
-            .stream()
-            .filter(i -> i == null)
-            .count();
+                            .stream()
+                            .filter(i -> i == null)
+                            .count();
         List<Integer> targets = raw
-            .stream()
-            .filter(i -> i != null)
-            .collect(Collectors.toList());
+                                .stream()
+                                .filter(i -> i != null)
+                                .collect(Collectors.toList());
         int count = 0;
         StopWatch watch = new StopWatch();
         Collections.sort(data);
 
         for (Integer target: targets) {
             watch.start();
-            if (Collections.binarySearch(data, target) >= 0) {
+            int binaryResult = Collections.binarySearch(data, target);
+            watch.stop();
+            if (binaryResult >= 0) {
                 ++count;
             }
-            watch.stop();
         }
-        
+
         makeOutput(4, count, targets.size() - count, invalidCount, watch);
     }
 
     /* Use interpolation search. Time it.
      */
     public static void experiment5(ArrayList<Integer> data)
-        throws IOException {
+    throws IOException {
         List<Integer> raw = readInput();
         long invalidCount = raw
-            .stream()
-            .filter(i -> i == null)
-            .count();
+                            .stream()
+                            .filter(i -> i == null)
+                            .count();
         List<Integer> targets = raw
-            .stream()
-            .filter(i -> i != null)
-            .collect(Collectors.toList());
+                                .stream()
+                                .filter(i -> i != null)
+                                .collect(Collectors.toList());
         int count = 0;
         StopWatch watch = new StopWatch();
 
         for (Integer target: targets) {
             watch.start();
-            if (interpolationSearch(data, target) >= 0) {
+            int interpolationResult = interpolationSearch(data, target);
+            watch.stop();
+            if (interpolationResult >= 0) {
                 ++count;
             }
-            watch.stop();
         }
-        
+
         makeOutput(5, count, targets.size() - count, invalidCount, watch);
+    }
+
+    /* Use the ArrayList of values to create a HashMap instance with a 50%
+     * load factor and an appropriate initial capacity. Next reopen the file
+     * on disk, and again process each value in turn, searching the HashMap
+     * for the given value. Measure and report the total time spent on
+     * searching for these values (not including I/O time to read or report).
+     */
+    public static void experiment6(ArrayList<Integer> data)
+    throws IOException {
+        HashMap<Integer, Integer> hash =
+            new HashMap<Integer, Integer>(data.size(), 0.5f);
+        List<Integer> raw = readInput();
+        long invalidCount = raw
+                            .stream()
+                            .filter(i -> i == null)
+                            .count();
+        List<Integer> targets = raw
+                                .stream()
+                                .filter(i -> i != null)
+                                .collect(Collectors.toList());
+        int count = 0;
+        StopWatch watch = new StopWatch();
+        for (Integer i: data) {
+            hash.put(i, 0);
+        }
+
+        for (Integer i: targets) {
+            watch.start();
+            boolean keyContained = hash.containsKey(i);
+            watch.stop();
+            if (keyContained) {
+                hash.put(i, hash.get(i)+1);
+                count++;
+            }
+        }
+
+        makeOutput(6, count, targets.size() - count, invalidCount, watch);
     }
 
     public static void debug(ArrayList<Integer> data) {
--- Assignment4/src/assignment4/StopWatch.java
+++ Assignment4/src/assignment4/StopWatch.java
@@ -1,72 +1,71 @@
 package assignment4;
 
-/**  
-   A stopwatch accumulates time when it is running. You can 
+/**
+   A stopwatch accumulates time when it is running. You can
    repeatedly start and stop the stopwatch. You can use a
    stopwatch to measure the running time of a program.
 */
-public class StopWatch
-{  
-   private long elapsedTime;
-   private long startTime;
-   private boolean isRunning;
-   public static final double NANOS_PER_SEC = 1000000000;
+public class StopWatch {
+    private long elapsedTime;
+    private long startTime;
+    private boolean isRunning;
+    public static final double NANOS_PER_SEC = 1000000000;
 
-   /**
-      Constructs a stopwatch that is in the stopped state
-      and has no time accumulated.
-   */
-   public StopWatch()
-   {  reset();
-   }
+    /**
+       Constructs a stopwatch that is in the stopped state
+       and has no time accumulated.
+    */
+    public StopWatch() {
+        reset();
+    }
 
-   /**
-      Starts the stopwatch. Time starts accumulating now.
-   */
-   public void start()
-   {  if (isRunning) return;
-      isRunning = true;
-      startTime = System.nanoTime();
-   }
-  
-   /**
-      Stops the stopwatch. Time stops accumulating and is
-      is added to the elapsed time.
-   */
-   public void stop()
-   {  if (!isRunning) return;
-      isRunning = false;
-      long endTime = System.nanoTime();
-      elapsedTime = elapsedTime + endTime - startTime;
-   }
-   
-   /**
-      Returns the total elapsed time.
-      @return the total elapsed time
-   */
-   public long getElapsedNanoseconds()
-   {  if (isRunning) 
-      {  long endTime = System.nanoTime ();
-         elapsedTime = elapsedTime + endTime - startTime;
-         startTime = endTime;
-      }
-      return elapsedTime;
-   }
+    /**
+       Starts the stopwatch. Time starts accumulating now.
+    */
+    public void start() {
+        if (isRunning) return;
+        isRunning = true;
+        startTime = System.nanoTime();
+    }
 
-   public double getElapsedSeconds() {
-       return getElapsedNanoseconds() / NANOS_PER_SEC;
-   }
-   
-   /**
-      Stops the watch and resets the elapsed time to 0.
-   */
-   public void reset()
-   {  elapsedTime = 0;
-      isRunning = false;
-   } 
+    /**
+       Stops the stopwatch. Time stops accumulating and is
+       is added to the elapsed time.
+    */
+    public void stop() {
+        if (!isRunning) return;
+        isRunning = false;
+        long endTime = System.nanoTime();
+        elapsedTime = elapsedTime + endTime - startTime;
+    }
+
+    /**
+       Returns the total elapsed time.
+       @return the total elapsed time
+    */
+    public long getElapsedNanoseconds() {
+        if (isRunning) {
+            long endTime = System.nanoTime ();
+            elapsedTime = elapsedTime + endTime - startTime;
+            startTime = endTime;
+        }
+        return elapsedTime;
+    }
+
+    public double getElapsedSeconds() {
+        return getElapsedNanoseconds() / NANOS_PER_SEC;
+    }
+
+    /**
+       Stops the watch and resets the elapsed time to 0.
+    */
+    public void reset() {
+        elapsedTime = 0;
+        isRunning = false;
+    }
 }
 
-//public class DemoStopWatch 
+//public class DemoStopWatch
 //{  /* here is a test driver program for the Stopwatch so that you can see
 //          how it is supposed to work  */
 //  public static void main(String[] args)
@@ -82,6 +81,6 @@ public class StopWatch
 //     mytime=s.getElapsedTime();
 //     System.out.println("Time elapsed in nanoseconds is: "+ mytime);
 //     System.out.println("Time elapsed in seconds is: "+ mytime/ NANOS_PER_SEC);
-//   
+//
 //  }
 //}
